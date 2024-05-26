c28082bc14eceda032fc48b02949dc2fdee4d967
Calvin James Maximo <calvin.james.12391@gmail.com>
2024-05-26 16:57:50 +0800

    feat: gender input (radio)

--- stress_level.py
+++ stress_level.py
@@ -16,5 +16,19 @@ st.title('Stress Level Prediction')
 def mlpModel():
     ...
 
-
 st.write('This machine learning project aims to predict the stress level being experienced by an individual given their sleep health and various lifestyle habits. The project uses a Multilayer Perceptron (MLP) as the machine learning algorithm.')
+
+st.divider()    # Add a divider
+
+st.header('Input data here')
+gender_col, age_col, sleep_dur_col, bmi_col = st.columns(4)
+heart_rate_col, steps_col, systolic_col, diastolic_col = st.columns(4)
+
+with gender_col:
+    st.subheader('Gender')
+    sex = st.radio("Please select your gender.", ["Male", "Female"], index=None)
+
+    if sex:
+        st.write(f'You selected {sex}')
+    else:
+        st.write('You have not selected your gender.')
